36a9fd47ad255d5f3e4e55557f1926a3175e77b1
israelr9897 <israelr9897@gmail.com>
2025-07-14 12:00:11 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { config } from "dotenv";
+import { configRout } from "./routs/configRout.js";
+config();
+const PORT = process.env.PORT;
+
+const app = express();
+
+configRout(app);
+
+app.listen(PORT, () => {
+  console.log("server is ronning on port:" + PORT);
+});
---
+++ routs/configRout.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+export function configRout(app){
+    app.use("/")
+}
